35c39814b6f41c412182993d03eec00829da87da
tbono <tbono@w2ogroup.com>
2019-04-22 17:34:48 -0400

    form validate complete!

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,34 @@
+import SimpleReactValidator from 'simple-react-validator'
 import React, { Component } from 'react';
-import {LiveCard} from './LiveCard'
-import {Person} from '../models/Person'
+import { LiveCard } from './LiveCard'
+import { Person, PersonValidators } from '../models/Person'
 import fire from './fire';
-import { Formik } from 'formik';
 
 export class App extends Component {
   constructor(props) {
     super(props);
-    //this.state = { users: [] }; // <- set up react state
-
     
-
     this.state = {
       user_model: new Person(),
     };
+
+    this.validator = new SimpleReactValidator({      
+      messages: {
+        // validator level custimization
+        //email: "That is not a valid email address."
+        //default: "Wompers! That's not right!"
+      },
+      validators: {
+        // ip: {
+        //   message: 'The :attribute must be a valid IP address.',
+        //   rule: function(val, params, validator) {
+        //     return validator.helpers.testRegex(val,/^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/i) && params.indexOf(val) === -1
+        //   }
+        // }
+      },
+      element: (message, className='') => <div className={className+" text-danger d-block"}>{message}</div>
+    });
+
 
 /*    this.state = {
       user_model:{
@@ -33,7 +48,11 @@ export class App extends Component {
 
     /*sets scope to the base class, not just from with in the method*/
     this.addRecord = this.addRecord.bind(this);  
+    this.submitForm = this.submitForm.bind(this);  
+    //this.changing = this.changing.bind(this);      
+    this.handleInputChange = this.handleInputChange.bind(this);      
   }
+  
   componentWillMount(){
 
       var S4 = function() {
@@ -50,29 +69,56 @@ export class App extends Component {
 
   }
 
-  addRecord(e){
-    e.preventDefault();        
-    fire.database().ref('users/'+this.state.user_model.uid).set(this.state.user_model);
-    
+  submitForm(e) {
+    e.preventDefault();  
+    if (this.validator.allValid()) {
+      alert('You submitted the form and stuff!');
+      this.addRecord();
+    } else {      
+      this.validator.showMessages();
+      // rerender to show messages for the first time
+      this.forceUpdate();      
+      this.validator.showMessages();
+    }
   }
-  changing(){        
+
+
+
+  addRecord(){      
+    fire.database().ref('users/'+this.state.user_model.uid).set(this.state.user_model);    
+  }
+
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
     this.setState({
       user_model:{
-        uid:        this.state.user_model.uid,
-        firstname : this.firstname.value,
-        lastname :  this.lastname.value,        
-        email :     this.email.value,
-        address :   this.address.value,
-        address2 :  this.address2.value,
-        city :      this.city.value,
-        states :    this.states.value,
-        zip :       this.zip.value,
-        agree :     this.agree.checked,
-        agree2 :    this.agree2.checked
+        ...this.state.user_model,
+        [name]: value,
       }
-      
-    })  
+    });
   }
+
+  // changing(){         
+  //   this.setState({
+  //     user_model:{
+  //       uid:        this.state.user_model.uid,
+  //       firstname : this.firstname.value,
+  //       lastname :  this.lastname.value,        
+  //       email :     this.email.value,
+  //       address :   this.address.value,
+  //       address2 :  this.address2.value,
+  //       city :      this.city.value,
+  //       states :    this.states.value,
+  //       zip :       this.zip.value,
+  //       agree :     this.agree.checked,
+  //       agree2 :    this.agree2.checked
+  //     }
+      
+  //   })  
+  // }
 
 
 
@@ -80,57 +126,64 @@ export class App extends Component {
   render() {
 
     
-
+    let user_model = this.state.user_model,
+        validators = new PersonValidators();        
 
     return (
       <div className="container">
       <div className="row">
         <div className="col-sm-4"> 
         
-        <form config={this.formConfig} ref="form" onSubmit={this.addRecord} onChange={this.changing.bind(this)}>
+        <form config={this.formConfig} ref="form" onSubmit={this.submitForm} >
 
           <div className="form-group row">
             <label className="col-12 col-form-label pb-0" htmlFor="firstname">First name</label> 
             <div className="col-12">
-              <input id="firstname" ref={ el => this.firstname = el } name="firstname" placeholder="" type="text" className="form-control" required="required"/>
+              <input id="firstname" name="firstname" placeholder="" value={user_model.firstname} onChange={this.handleInputChange} type="text" className="form-control" />
+              {this.validator.message('firstname', user_model.firstname, validators.firstname.rules, { messages:validators.firstname.messages }) }
             </div>
           </div>
 
           <div className="form-group row">
             <label className="col-12 col-form-label pb-0" htmlFor="lastname">Last Name</label> 
             <div className="col-12">
-              <input id="lastname" ref={ el => this.lastname = el } name="lastname" placeholder="" type="text" className="form-control" required="required"/>
+              <input id="lastname" name="lastname" placeholder="" value={user_model.lastname} onChange={this.handleInputChange} type="text" className="form-control" />
+              {this.validator.message('lastname', user_model.lastname, validators.lastname.rules, { messages:validators.lastname.messages }) }
             </div>
           </div>
 
           <div className="form-group row">
             <label htmlFor="email" className="col-12 col-form-label pb-0">Email</label> 
             <div className="col-12">
-              <input id="email" ref={ el => this.email = el } name="email" placeholder="" type="text" className="form-control" required="required"/>
+              <input id="email" name="email" placeholder="" value={user_model.email} onChange={this.handleInputChange} type="text" className="form-control" />
+                {this.validator.message('email', user_model.email, validators.email.rules, { className:'differentErrorClass',messages:validators.email.messages }) }
             </div>
           </div>
           <div className="form-group row">
             <label htmlFor="address" className="col-12 col-form-label pb-0">Address</label> 
             <div className="col-12">
-              <input id="address" ref={ el => this.address = el } name="address" placeholder="123 Main St. Anytown New Jersey 07073" type="text" required="required" className="form-control"/>
+              <input id="address" name="address" placeholder="123 Main St. Anytown New Jersey 07073" value={user_model.address} onChange={this.handleInputChange} type="text"  className="form-control"/>
+              {this.validator.message('address', user_model.address, validators.address.rules, { messages:validators.address.messages }) }
             </div>
           </div>
           <div className="form-group row">
             <label htmlFor="address2" className="col-12 col-form-label pb-0">Address 2</label> 
             <div className="col-12">
-              <input id="address2" ref={ el => this.address2 = el } name="address2" placeholder="Appt 309" type="text" className="form-control"/>
+              <input id="address2" name="address2" placeholder="Appt 309" value={user_model.address2} onChange={this.handleInputChange} type="text" className="form-control"/>
+              {this.validator.message('address2', user_model.address2, validators.address2.rules, { messages:validators.address2.messages }) }
             </div>
           </div>
           <div className="form-group row">
             <label htmlFor="city" className="col-12 col-form-label pb-0">City</label> 
             <div className="col-12">
-              <input id="city" ref={ el => this.city = el } name="city" placeholder="" type="text" className="form-control" required="required"/>
+              <input id="city" name="city" placeholder="" value={user_model.city} onChange={this.handleInputChange} type="text" className="form-control" />
+              {this.validator.message('city', user_model.city, validators.city.rules, { messages:validators.city.messages }) }
             </div>
           </div>
           <div className="form-group row">
             <label htmlFor="state" className="col-12 col-form-label pb-0">State</label> 
             <div className="col-12">
-              <select id="state" ref={ el => this.states = el } name="state" className="custom-select" aria-describedby="stateHelpBlock" required="required">
+              <select id="state" name="state" className="custom-select" aria-describedby="stateHelpBlock" value={user_model.state} onChange={this.handleInputChange} >
                 <option value="">Select a state</option>
                 <option value="AL">Alabama</option>
                 <option value="AK">Alaska</option>
@@ -183,20 +236,23 @@ export class App extends Component {
                 <option value="WV">West Virginia</option>
                 <option value="WI">Wisconsin</option>
                 <option value="WY">Wyoming</option>
-              </select>               
+              </select>
+              {this.validator.message('state', user_model.state, validators.state.rules, { messages:validators.state.messages }) }               
             </div>
           </div>
           <div className="form-group row">
             <label htmlFor="zip" className="col-12 col-form-label pb-0">Zip</label> 
             <div className="col-12">
-              <input id="zip" ref={ el => this.zip = el } name="zip" placeholder="" type="text" className="form-control" required="required"/>
+              <input id="zip" name="zip" placeholder="" value={user_model.zip} onChange={this.handleInputChange} type="text" className="form-control" />
+              {this.validator.message('zip', user_model.zip, validators.zip.rules, { messages:validators.zip.messages }) }               
             </div>
           </div>
           <div className="form-group row">
             <div className="col-12"></div> 
             <div className="col-12">
               <div className="custom-control custom-checkbox custom-control-inline">
-                <input name="agree" ref={ el => this.agree = el } id="agree" type="checkbox" className="custom-control-input" value="" required="required"/> 
+                <input name="agree" id="agree" type="checkbox" className="custom-control-input" value={user_model.agree} onChange={this.handleInputChange} /> 
+                {this.validator.message('agree', user_model.agree, validators.agree.rules, { messages:validators.agree.messages }) } 
                 <label htmlFor="agree" className="custom-control-label">I agree to something</label>
               </div>
             </div>
@@ -205,7 +261,8 @@ export class App extends Component {
             <div className="col-12"></div> 
             <div className="col-12">
               <div className="custom-control custom-checkbox custom-control-inline">
-                <input name="agree2" ref={ el => this.agree2 = el } id="agree2" type="checkbox" className="custom-control-input" value=""/> 
+                <input name="agree2" id="agree2" type="checkbox" className="custom-control-input" value={user_model.agree2} onChange={this.handleInputChange}/> 
+                {this.validator.message('agree2', user_model.agree2, validators.agree2.rules, { messages:validators.agree2.messages }) }
                 <label htmlFor="agree2" className="custom-control-label">I agree to something but not required</label>
               </div>
             </div>
--- src/models/Person.js
+++ src/models/Person.js
@@ -1,4 +1,5 @@
 export class Person {
+
     constructor(user_obj = {}) {      
         //this.uid = ('uid' in user_obj) ? user_obj.uid : '';       
         this.uid = '';       
@@ -14,12 +15,76 @@ export class Person {
         this.agree2 = '';
     }
 }
-/*export const config = {
-    firstname: {
-        isRequired: 'First name is required!',
-    },
-    lastname: {
-        isRequired: 'Last name is required!',
-    },
 
-};*/
+export class PersonValidators {
+
+    constructor(){
+        return{
+            firstname:{
+                rules:'required',
+                messages:{
+                    required:'Custom required message first name',
+                }
+            },
+            lastname:{
+                rules:'required',
+                messages:{
+                    required:'Custom required message last name',
+                }
+            },
+            email:{
+                rules:'required|email',
+                messages:{
+                    required:'Custom required message email',
+                    email:'Custom email message email',
+                }
+            },
+            address:{
+                rules:'required',
+                messages:{
+                    required:'Custom required message address',                    
+                }
+            },
+            address2:{
+                rules:'required',
+                messages:{
+                    required:'Custom required message address2',                    
+                }
+            },
+            city:{
+                rules:'required',
+                messages:{
+                    required:'Custom required message city',                    
+                }
+            },
+            state:{
+                rules:'required',
+                messages:{
+                    required:'Custom required message state',                    
+                }
+            },
+            zip:{
+                rules:'required|alpha_num|max:5|min:5',
+                messages:{
+                    required:'Custom required message zip',                    
+                    alpha_num:'Custom alpha_num message zip',                    
+                    min:'Custom min 5 message zip',                    
+                    max:'Custom max 5 message zip',                    
+                }
+            },
+            agree:{
+                rules:'required',
+                messages:{
+                    required:'Custom required message agree',                    
+                }
+            },
+            agree2:{
+                rules:'required',
+                messages:{
+                    required:'Custom required message agree2',                    
+                }
+            },
+        }
+    }   
+    
+}
